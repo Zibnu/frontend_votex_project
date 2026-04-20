11379843cb91d285622af16e92a0b3fad41f6c7b
Zibnu <crimhomid@gmail.com>
2026-04-21 06:10:27 +0700

    trouble in render page because mui-x chart

--- frontend_votex/src/Admin/pages/DashboardPage.jsx
+++ frontend_votex/src/Admin/pages/DashboardPage.jsx
@@ -62,8 +62,107 @@ function DashboardPage() {
 
     const barLabels = data.votesPerCandidate.map((item) => `${item.ketua_name}`);
     const barValues = data.votesPerCandidate.map((item) => `${item.total_votes}`);
+
     return (
-        <div>DashboardPage</div>
+        <div className='p-6 space-y-6'>
+            <div className="bg-white p-4 rounded-xl shadow">
+                <h2 className="text-xl font-semibold">Dashboard Page</h2>
+            </div>
+
+            <div className="grid grid-cols-4 gap-4">
+                <motion.div 
+                whileHover={{scale : 1.03}}
+                className="bg-white p-4 rounded-xl shadow flex items-center gap-4">
+                    <FaUsers size={40}/>
+                    <div>
+                        <p className="text-sm text-[#37474F]">Total Voters</p>
+                        <h2 className="text-xl font-bold text-[#1E88E5]">
+                            {data.totalUser}
+                        </h2>
+                    </div>
+                </motion.div>
+
+                <motion.div 
+                whileHover={{scale : 1.03}}
+                className="bg-white p-4 rounded-xl shadow flex items-center gap-4">
+                    <ImUserCheck size={40}/>
+                    <div>
+                        <p className="text-sm text-[#37474F]">Have Chosen</p>
+                        <h2 className="text-xl font-bold text-[#2E7D32]">
+                            {data.totalVoted}
+                        </h2>
+                    </div>
+                </motion.div>
+
+                <motion.div 
+                whileHover={{scale : 1.03}}
+                className="bg-white p-4 rounded-xl shadow flex items-center gap-4">
+                    <TbUserQuestion size={40}/>
+                    <div>
+                        <p className="text-sm text-[#37474F]">Not Voted</p>
+                        <h2 className="text-xl font-bold text-[#F57C00]">
+                            {data.totalNotVoted}
+                        </h2>
+                    </div>
+                </motion.div>
+
+                <motion.div 
+                whileHover={{scale : 1.03}}
+                className="bg-white p-4 rounded-xl shadow flex items-center gap-4">
+                    <TbUserQuestion size={40}/>
+                    <div>
+                        <p className="text-sm text-[#37474F]">Candidate</p>
+                        <h2 className="text-xl font-bold text-[#673AB7]">
+                            {data.totalCandidate}
+                        </h2>
+                    </div>
+                </motion.div>
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+                <div className="bg-white p-4 rounded-xl shadow">
+                    <h2 className="mb-2 font-semibold">Voting Result</h2>
+
+                    <PieChart
+                        series={[
+                            {
+                                data : pieData
+                            },
+                        ]}
+                        width={300}
+                        height={200}
+                    />
+                </div>
+
+                <div className="bg-white p-4 rounded-xl shadow flex flex-col justify-center items-center">
+                    <h2 className="font-semibold text-[#37474F]">Percantage Have Voted</h2>
+
+                    <h1 className="text-4xl font-bold text-[#2E7D32] mt-2">
+                        {data.percentage}%
+                    </h1>
+
+                    <p className="text-[#37474F] text-sm"> 
+                        from all voters
+                    </p>
+                </div>
+
+            </div>
+
+            <div className="bg-white p-4 rounded-xl shadow">
+                <h2 className="mb-4 font-semibold">Votes Per Candidate</h2>
+
+                <BarChart
+                xAxis={{ scaleType : "band", data : barLabels}}
+                series={[
+                    {
+                        data : barValues,
+                    },
+                ]}
+                width={500}
+                height={300}
+                />
+            </div>
+        </div>
     )
 }
 
--- frontend_votex/src/routes/AppRouter.jsx
+++ frontend_votex/src/routes/AppRouter.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Login from '../pages/Login';
 import Vote from '../pages/Vote';
 import SuccessVote from '../pages/SuccessVote';
+import AdminLayout from "../layouts/AdminLayout"
+import DashboardPage from '../Admin/pages/DashboardPage';
 
 function AppRouter() {
     return (
@@ -11,6 +13,10 @@ function AppRouter() {
 
             <Route path='/vote' element={<Vote/>} />
             <Route path='/success_voted' element={<SuccessVote/>} />
+
+            <Route element={<AdminLayout/>}>
+                <Route path='/admin/dashboard' element={<DashboardPage/>} />
+            </Route>
         </Routes>
     )
 }
